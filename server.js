3f2a022de0296b865a02255086716558caa22e10
Alabi Emmanuel <bobslegend795@gmail.com>
2018-07-01 12:13:50 +0100

    Version 1.0.0

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 const config = require("./config/keys");
 const items = require("./routes/api/items");
@@ -18,5 +19,15 @@ mongoose.connect(config.mongoURI).then(() => console.log("MongoDB connected")).c
 
 // Use Routes
 app.use("/api/items", items);
+
+// Serve static asset if in prod
+if(process.env.NODE_ENV === "production") {
+    // Set static folder
+    app.use(express.static("client/build"));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    })
+}
 
 app.listen(PORT, () => console.log(`Server started on port ${ PORT }`));
